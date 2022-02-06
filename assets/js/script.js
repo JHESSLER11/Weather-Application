3958390fa7e751b2c37d40d41af48a54a9ee45fc
JHessler <jhessler11@gmail.com>
2022-02-06 18:44:27 -0500

    added weather api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,16 @@
 
+
+function fetchApi() {
+    var apiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=56cd55bcb41fb1d5dd1158c24bb37cc0'
+
+   $.ajax(apiUrl).done(function(response) {
+       console.log(response)
+   });
+};
+
+fetchApi()
+
+
 $(".searchBtn").on("click", function() {
         var city = $(this).siblings(".citySearch").val();
         var what = $(this).parent().attr('id');
