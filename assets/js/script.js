b14a83d2d7707c833bc880c220abf99b4c9b5ded
JHessler <jhessler11@gmail.com>
2022-02-08 19:16:02 -0500

    fixed API request

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,49 @@
 const apiKey = '56cd55bcb41fb1d5dd1158c24bb37cc0';
-const searchCity = $('.citySearch');
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${apiKey}`
-
-
-fetch(url)
-.then(function (response) {
-    let data = response.json()
-    return data
-})
-.then(function(data) {
-
-});
-    //msg.textContent = "search for a real city";
+const searchedCities = []; //stringify the local storage
+//const searchCity = $('.citySearch');
+//const searchCity = $('#searchForm .citySearch').val(localStorage.getItem("searchForm"))
+//msg.textContent = "search for a real city";
 // function fetchApi() {
-//     var apiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=asheville,NC,US&limit=56cd55bcb41fb1d5dd1158c24bb37cc';
+    //     var apiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=asheville,NC,US&limit=56cd55bcb41fb1d5dd1158c24bb37cc';
+    
+    //    $.ajax(apiUrl).done(function(response) {
+        //        console.log(response)
+        //    });
+        // };
+        
+        //fetchApi()
+        
+        
+        $(".searchBtn").on("click", function(event) {
+            event.preventDefault() //keeps page from refeshing 
+            //calls the search item
+            var city = $(this).siblings(".citySearch").val();
+            //var cities = $(this).parent().attr('id');
+            
+            searchedCities.push(city);
+            //puts the value in local storage
+            localStorage.setItem("cities", city)
+            
 
-//    $.ajax(apiUrl).done(function(response) {
-//        console.log(response)
-//    });
-// };
 
-//fetchApi()
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+            
+            console.log(url)
+            fetch(url)
+            .then(function (response) {
+                let data = response.json()
+                return data
+            })
+            .then(function(data) {
+            console.log(data)
+            });
+            
+        });
 
 
-$(".searchBtn").on("click", function() {
-        var city = $(this).siblings(".citySearch").val();
-        var what = $(this).parent().attr('id');
-        localStorage.setItem(city, what)
-});
 
 //$('#searchContainer .citySearch').val(localStorage.getItem("searchContainer"))
 
-var pastSearches = [];
-    pastSearches.push($('.citySearch').val());
+//var pastSearches = [];
+   // pastSearches.push($('.citySearch').val());
     
