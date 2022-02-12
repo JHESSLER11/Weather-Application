1de00c7244b3926124e23778a4aec78fd7357f4e
JHessler <jhessler11@gmail.com>
2022-02-12 10:43:33 -0500

    button element addded

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 const apiKey = '56cd55bcb41fb1d5dd1158c24bb37cc0';
 const searchedCities = []; //stringify the local storage
 var cCounter = 0
+const historyDataCityAttr = "data-city";
+const historyBtnBsClasses = "btn btn-dark border text-left";
 //const searchCity = $('.citySearch');
 //const searchCity = $('#searchForm .citySearch').val(localStorage.getItem("searchForm"))
 //msg.textContent = "search for a real city";
@@ -13,17 +15,18 @@ var cCounter = 0
         // };
         
         //fetchApi()
+ //search city API        
+function searchWeather (city) {
         
-        
-        $(".searchBtn").on("click", function(event) {
-            event.preventDefault() //keeps page from refeshing 
+            //event.preventDefault() //keeps page from refeshing 
             //calls the search item
-            var city = $(this).siblings(".citySearch").val();
+            //var city = $(".citySearch").val();
             //var cities = $(this).parent().attr('id');
             //if statement that will check the array for duplicate cities
-            searchedCities.push(city);
             //puts the value in local storage
-            localStorage.setItem("cities", city)
+            //searchedCities.push(city);
+            //localStorage.setItem("cities", city)
+            //console.log(city)
             
 
 
@@ -38,19 +41,42 @@ var cCounter = 0
             .then(function(data) {
             console.log(data)
             });
-            
-        });
 
-var searchEl = function() {
-    var btnEl = document.createElement('button')
-    btnEl.className = "btn btn-second"
-    btnEl.textContent = document.querySelector("input[name='city']")
-    btnEl.setAttribute('data-city-id', cCounter);
-    btnEl.setAttribute('id', 'searchBtn');
-    btnEl.setAttribute('type', "submit");
+        };
+        $("#searchForm").on("submit", handleSearch)
+
+
+//displays the searched for city 
+function searchEl() {
+    var recentSearch = $('.recentSearch')
+    searchedCities.forEach((city) => {
+        var btnEl = $("<button>").addClass(historyBtnBsClasses);
+        btnEl.attr(historyDataCityAttr, city);
+        btnEl.text(city);
+        recentSearch.append(btnEl);
+
+    })
+    // btnEl.className = "btn btn-second"
+    // btnEl.textContent = document.querySelector("input[name='cities']")
+    // btnEl.setAttribute('data-city-id', cCounter);
+    // btnEl.setAttribute('id', 'searchBtn');
+    // btnEl.setAttribute('type', "submit");
 }
+
+function handleSearch(event) {
+
+    event.preventDefault();
+    var city = $(".citySearch").val();
+    searchedCities.push(city);
+    localStorage.setItem("cities", city)
+    
+    searchWeather(city)
+    searchEl()
+
+}
+//takes cities from local storage and displays them on the page with refresh
 var citieStorage = function() {
-    var searchList = JSON.parse(localStorage.getItem('city'))
+    var searchList = JSON.parse(localStorage.getItem('cities'))
 
     for (let index = 0; index < searchList.length; index++) {
         var btnEl = document.createElement('button')
@@ -64,12 +90,12 @@ var citieStorage = function() {
 }
 
 
-if (localStorage.getItem('city')) {
-    JSON.parse(cities = localStorage.getItem('city'))
-}
-if (searchedCities.length !==0) {
-    loadSearch();
-}
+// if (localStorage.getItem('cities')) {
+//     JSON.parse(cities = localStorage.getItem('cities'))
+// }
+// if (searchedCities.length !==0) {
+//     citieStorage();
+// }
 //for loop through searchcities to display them. 
 
 //$('#searchContainer .citySearch').val(localStorage.getItem("searchContainer"))
